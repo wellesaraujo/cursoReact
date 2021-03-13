0a8423c9de1ddc45ffd9f8c2def02502e84856af
Wellington Araujo <welles.araujo@hotmail.com>
2021-03-13 18:26:04 -0300

    Exibindo informacoes da equipe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import React from 'react';
 
-const BemVindo = (props) => {
-  return(
-    <div>
-        <h2>Bem-vindx: {props.nome}!</h2>
-        <h3>Tenho {props.idade} anos.</h3>
-    </div>
-  );  
-}
-function App (){
+const Equipe = (props) => {
     return (
         <div>
-            <h1>Olá Mundo!</h1>
-            <BemVindo nome="Welles" idade="23"/>
-            <BemVindo nome="Amanda" idade="15"/>
+            <Sobre
+                nome={props.nome}
+                cargo={props.cargo}
+                idade={props.idade} />
+            <Social fb={props.fb}/>
+            <hr/>
         </div>
-        
     );
-    
+}
+
+const Sobre = (props) => {
+    return (
+        <div>
+            <h2>Olá, eu sou {props.nome}</h2>
+            <h2>Idade: {props.idade} anos</h2>
+            <h2>Cargo: {props.cargo}</h2>
+        </div>
+    );
+}
+
+const Social = (props) => {
+    return(
+        <div>
+            <a href={props.fb}>Facebook </a>
+            <a>Instagram </a>
+            <a>GitHub </a>
+        </div>
+    )
+}
+function App() {
+    return (
+        <div>
+            <h1>Conheça nossa equipe:</h1>
+            <hr/>
+            <Equipe
+                nome="Wellington"
+                cargo="Dev"
+                idade="26"
+                fb="https://www.facebook.com/WellesAraujo/"
+            />
+            <Equipe
+                nome="Kawany"
+                cargo="Design"
+                idade="23"
+            />
+        </div>
+
+    );
+
 }
 
 export default App;
