911b0097ee6c654892c9b17faa2aee24b046fe6a
Wellington Araujo <welles.araujo@hotmail.com>
2021-03-13 16:27:18 -0300

    Componentes e props estaticas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+const BemVindo = (props) => {
+  return(
+    <div>
+        <h2>Bem-vindx: {props.nome}!</h2>
+        <h3>Tenho {props.idade} anos.</h3>
+    </div>
+  );  
+}
 function App (){
     return (
         <div>
             <h1>Olá Mundo!</h1>
+            <BemVindo nome="Welles" idade="23"/>
+            <BemVindo nome="Amanda" idade="15"/>
         </div>
         
     );
